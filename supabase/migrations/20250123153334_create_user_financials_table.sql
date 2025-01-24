128932697fc5c9e6ab8127ef498c65b1560f9bfa
kainatori <kai_natori@icloud.com>
2025-01-24 13:50:41 +0800

    chore: add user_financials table

---
+++ supabase/migrations/20250123153334_create_user_financials_table.sql
@@ -0,0 +1,36 @@
+create table
+  user_financials (
+    id uuid references auth.users on delete cascade not null primary key,
+    stripe_customer_id text unique,
+    pending_balance bigint not null default 0,
+    balance bigint not null default 0
+  );
+
+alter table user_financials enable row level security;
+
+create policy "Enable users to view their own data balance only" on user_financials for
+select
+  using (auth.uid () = id);
+
+revoke all on user_financials
+from
+  public;
+
+grant
+select
+  (id, pending_balance, balance) on user_financials to authenticated;
+
+create
+or replace function public.handle_new_user () returns trigger
+set
+  search_path = '' as $$
+begin
+  insert into public.profiles (id, name, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'name', new.raw_user_meta_data->>'avatar_url');
+
+  insert into public.user_financials (id)
+  values (new.id);
+
+  return new;
+end;
+$$ language plpgsql security definer;
