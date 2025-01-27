6db996b60888c775c456a3450e17d150399e7c26
kai natori <kai_natori@icloud.com>
2025-01-27 22:31:13 +0800

    fix: enviroment variabl name

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -3,8 +3,7 @@ import type { Database } from "../../../supabase/types";
 
 export async function supabaseAdminClient() {
 	const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-	const supabaseServiceRoleKey =
-		process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
+	const supabaseServiceRoleKey = process.SUPABASE_SERVICE_ROLE_KEY;
 
 	if (!supabaseUrl || !supabaseServiceRoleKey) {
 		throw new Error("Supabase URL or Service Role Key is not set");
